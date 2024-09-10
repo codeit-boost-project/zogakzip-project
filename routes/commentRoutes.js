1e0c54cb95b8c569bf4cdc31a3ea9b1fb3f60ec3
bbstation09 <tw2ysbe@naver.com>
2024-09-10 15:51:24 +0900

    [update] routes files

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -10,8 +10,8 @@ const router = express.Router();
 
 /* 특정 게시물에서의 댓글 등록, 댓글 목록 조회 */
 
-router.post('/comments/:postId/comments', registerComment); // 댓글 등록 -> 완료
-router.get('/comments/:postId/comments', viewCommentList); // 댓글 목록 조회 -> 완료
+router.post('/posts/:postId/comments', registerComment); // 댓글 등록 -> 완료
+router.get('/posts/:postId/comments', viewCommentList); // 댓글 목록 조회 -> 완료
 router.delete('/comments/:commentId', deleteComment); // 댓글 삭제 -> 완료
 router.put('/comments/:commentId', editComment); // 댓글 수정 -> 완료
 
