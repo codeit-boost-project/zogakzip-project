5df98ebf401cd93fbc90a379e88b7dfb73f2489f
bbstation09 <tw2ysbe@naver.com>
2024-09-12 17:50:03 +0900

    [update] mock.js imageUrl

--- prisma/mock.js
+++ prisma/mock.js
@@ -1,7 +1,7 @@
 export const GROUPS = [
   {
     name: "달봉이네 가족",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "달봉이네 추억 상자입니다. 가족의 소중한 기억들을 함께 나눠요.",
     isPublic: true,
     password: "family123",
@@ -11,7 +11,7 @@ export const GROUPS = [
   },
   {
     name: "여행 애호가들",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "여행지에서의 추억을 나누는 공간입니다.",
     isPublic: true,
     password: "travel123",
@@ -21,7 +21,7 @@ export const GROUPS = [
   },
   {
     name: "스포츠 팬클럽",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "스포츠와 관련된 추억을 나누는 팬클럽입니다.",
     isPublic: true,
     password: "sports123",
@@ -31,7 +31,7 @@ export const GROUPS = [
   },
   {
     name: "독서 클럽",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "읽었던 책과 느꼈던 감상을 기억해요.",
     isPublic: false,
     password: "book123",
@@ -41,7 +41,7 @@ export const GROUPS = [
   },
   {
     name: "등산 동호회",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "등산의 순간과 풍경을 함께 기억하는 모임입니다.",
     isPublic: true,
     password: "hike123",
@@ -51,7 +51,7 @@ export const GROUPS = [
   },
   {
     name: "음악 감상",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "음악으로 남긴 기억들을 공유합니다.",
     isPublic: false,
     password: "music123",
@@ -61,7 +61,7 @@ export const GROUPS = [
   },
   {
     name: "사진 동호회",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "사진을 통해 남긴 추억을 나누는 그룹입니다.",
     isPublic: true,
     password: "photo123",
@@ -71,7 +71,7 @@ export const GROUPS = [
   },
   {
     name: "요가 클래스",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "요가 수련 중 느낀 순간들을 기억합니다.",
     isPublic: true,
     password: "yoga123",
@@ -81,7 +81,7 @@ export const GROUPS = [
   },
   {
     name: "영화 감상 클럽",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "영화를 통해 느꼈던 감동을 기록하고 공유합니다.",
     isPublic: false,
     password: "movie123",
@@ -91,7 +91,7 @@ export const GROUPS = [
   },
   {
     name: "요리 연구회",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     introduction: "다양한 요리와 그 순간을 기억하는 모임입니다.",
     isPublic: true,
     password: "cook123",
@@ -105,7 +105,7 @@ export const POSTS = [
   {
     nickname: "달봉이",
     title: "오늘 인천에서 월척을 낚았어요!",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "인천에서 가족과 함께한 낚시에서 월척을 낚았어요. 모두가 놀랐던 순간!",
     tags: "가족, 낚시, 인천",
     location: "인천, 한국",
@@ -119,7 +119,7 @@ export const POSTS = [
   {
     nickname: "Traveler1",
     title: "파리에서의 아름다운 저녁",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "에펠탑을 배경으로 한 아름다운 저녁. 잊을 수 없는 순간입니다.",
     tags: "여행, 파리",
     location: "파리, 프랑스",
@@ -133,7 +133,7 @@ export const POSTS = [
   {
     nickname: "SportyFan",
     title: "2024 올림픽 경기 관람",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "2024 올림픽에서의 경기 관람. 현장의 열기가 대단했어요!",
     tags: "올림픽, 경기, 스포츠",
     location: "파리, 프랑스",
@@ -147,7 +147,7 @@ export const POSTS = [
   {
     nickname: "BookLover",
     title: "첫 독서 모임의 추억",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "독서 모임에서 함께했던 첫날. 다양한 이야기를 나눴던 그 시간.",
     tags: "독서, 모임",
     location: "부산, 한국",
@@ -161,7 +161,7 @@ export const POSTS = [
   {
     nickname: "HikerJoe",
     title: "설악산 등반의 기쁨",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "친구들과 함께한 설악산 등반. 정상에서의 기쁨은 말로 표현할 수 없어요.",
     tags: "등산, 설악산",
     location: "강원도, 한국",
@@ -175,7 +175,7 @@ export const POSTS = [
   {
     nickname: "MusicFan",
     title: "첫 콘서트에서의 감동",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "어렸을 때 처음 갔던 콘서트. 그날의 감동은 아직도 생생해요.",
     tags: "음악, 콘서트",
     location: "뉴욕, 미국",
@@ -189,7 +189,7 @@ export const POSTS = [
   {
     nickname: "PhotoGrapher",
     title: "첫 사진 전시회",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "제가 찍은 사진들이 전시된 첫 전시회. 정말 잊을 수 없는 기억이에요.",
     tags: "사진, 전시회",
     location: "서울, 한국",
@@ -203,7 +203,7 @@ export const POSTS = [
   {
     nickname: "YogaLover",
     title: "해변에서의 아침 요가",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "바닷바람을 맞으며 즐긴 해변에서의 아침 요가. 최고의 힐링 시간이었어요.",
     tags: "요가, 해변",
     location: "하와이, 미국",
@@ -217,7 +217,7 @@ export const POSTS = [
   {
     nickname: "MovieBuff",
     title: "영화 '기생충' 상영회",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "친구들과 함께 한 기생충 상영회. 다시 보니 더욱 의미가 깊었어요.",
     tags: "영화, 기생충",
     location: "서울, 한국",
@@ -231,7 +231,7 @@ export const POSTS = [
   {
     nickname: "ChefMaster",
     title: "최고의 요리 경연대회",
-    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171",
+    imageUrl: "https://web-project-mkuy.onrender.com/uploads/1725426705171.jpg",
     content: "요리 경연대회에서 우승했던 순간. 그날의 환호성과 기쁨이 아직도 느껴져요.",
     tags: "요리, 경연대회",
     location: "도쿄, 일본",
